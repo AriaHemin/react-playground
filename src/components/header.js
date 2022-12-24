ae9dbafb48a595440dacce31164bc1594d3d90da
Aria Hemin <ariahemin@gmail.com>
2022-12-24 11:48:39 +0300

    minor edits

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 function Header(){
-  return <header>
-    <button class='headerItem'>app 1</button>
-    <button class='headerItem'>app 2</button>
-    <button class='headerItem'>app 3</button>
-    <button class='headerItem'>app 4</button>
-  </header>;
+  return (<header>
+    <button class="headerItem" >app 1</button>
+    <button class="headerItem" >app 2</button>
+    <button class="headerItem" >app 3</button>
+    <button class="headerItem" >app 4</button>
+  </header>
+  );
 }
 
 export default Header;
