0de5f5dedb30f4073b781c550a14cd2ac9458f7c
Aria Hemin <ariahemin@gmail.com>
2022-12-25 16:25:17 +0300

    i am going to use these commits to update about the projects but also explain so i learn better. what i did is i made an array of objects for our characters using the useState hook method. and we have name power and img key. and bellow in the line 33 we have a div inside of that we use map method on the characters array to basically apply same thing to all array items. basically we have an arrow function that takes easy array item which is an object for our hero and asigns it as a attribute to the component to then make the cards.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import './index.css';
-
-import App1irwyr from './components/irwyrApp';
+import Character from './components/character';
+import { useState } from 'react';
 
 
 function App() {
+  const [characters, setCharacter] = useState(
+    [
+      {
+        name: "batman", 
+        power: "really rich", 
+        img : "https://images.pexels.com/photos/11791435/pexels-photo-11791435.jpeg" 
+      },
+      {
+        name: "robin", 
+        power: "athletic", 
+        img : "https://images.pexels.com/photos/10228068/pexels-photo-10228068.jpeg" 
+      },
+      {
+        name: "bugs bunny", 
+        power: "comic", 
+        img : "https://images.pexels.com/photos/4588056/pexels-photo-4588056.jpeg" 
+      },
+      {
+        name: "spiderman", 
+        power: "spider power", 
+        img : "https://images.pexels.com/photos/8421963/pexels-photo-8421963.jpeg" 
+      }
+    ]
+  );
   return (
     <div className="App">
-      <App1irwyr/>
+      <div className='flex flex-wrap justify-center '>
+        {characters.map((character) => {
+          return(
+          <Character 
+            name={character.name} 
+            power={character.power} 
+            img={character.img} 
+          />)
+        })}
+      </div>
     </div>
   );
 }
