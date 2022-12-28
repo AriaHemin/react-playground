2f0cae88f13a5f2e98f7ce48300a22e4186f3527
Aria Hemin <ariahemin@gmail.com>
2022-12-28 14:12:16 +0300

    trying out hashRouter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './index.css';
 import TodoApp from "./pages/todoApp";
 import Home from "./pages/home"
 import Appiwwyw from './pages/iwwywApp';
-import {BrowserRouter , Routes , Route} from "react-router-dom";
+import {BrowserRouter , Routes , Route , HashRouter} from "react-router-dom";
 
 
 function App() {
         return(
             <>
-                <BrowserRouter>
+                <HashRouter>
                     <Routes>
-                        <Route path='/react-playground' element={<Home/>} />
-                        <Route path='/todoApp' element={<TodoApp/>} />
-                        <Route path='/iwwywApp' element={<Appiwwyw/>}/>
+                        <Route exact path='/react-playground' element={<Home/>} />
+                        <Route exact path='/todoApp' element={<TodoApp/>} />
+                        <Route exact path='/iwwywApp' element={<Appiwwyw/>}/>
                     </Routes>
-                </BrowserRouter>
+                </HashRouter>
             </>
     )
 }       
